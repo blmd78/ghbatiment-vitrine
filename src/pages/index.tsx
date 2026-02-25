e707aa1aaa4ec0a8e7a600682382242f50af9652
Benoit <blmd8345@gmail.com>
2026-02-25 15:19:28 +0100

    fix bug white space on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -494,7 +494,7 @@ export default function Home() {
       </section>
 
       {/* Nos Engagements Section */}
-      <section className="py-24 lg:py-32 bg-white">
+      <section className="py-24 lg:py-32 bg-white overflow-hidden">
         <div className="container-wide">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center">
             {/* Content */}
